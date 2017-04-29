6d6879de7a7116a495655d48095b520a06730d10
Felipe Cauê Legal Benedito <felipecaue.lb@gmail.com>
2017-04-29 01:05:50 -0300

    Exercício 2

--- ex123/ex2.php
+++ ex123/ex2.php
@@ -11,15 +11,15 @@
             <h3 class="page-header">Antes</h3>
             
             <pre>
-            &lt;?
+                &lt;?
 
-                if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-                    header("Location: http://www.google.com");
-                    exit();
-                } elseif (isset($_COOKIE['Loggedin']) && $_COOKIE['Loggedin'] == true) {
-                    header("Location: http://www.google.com");
-                    exit();
-                }
+                    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+                        header("Location: http://www.google.com");
+                        exit();
+                    } elseif (isset($_COOKIE['Loggedin']) && $_COOKIE['Loggedin'] == true) {
+                        header("Location: http://www.google.com");
+                        exit();
+                    }
             </pre>
         </div>
         
@@ -27,12 +27,16 @@
             <h3 class="page-header">Depois</h3>
             
             <pre>
-            &lt;?php
+                &lt;?php
 
-                if ( (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) ) || 
-                   ( (isset($_COOKIE['Loggedin']) && $_COOKIE['Loggedin'] == true) ) {
-                    header("Location: http://www.google.com");
-                }
+                    if (
+                           ( isset($_SESSION['loggedin']) and $_SESSION['loggedin'] === true )
+                           or
+                           ( isset($_COOKIE['Loggedin']) and $_COOKIE['Loggedin'] === true )
+                        )
+                    {
+                        header("Location: http://www.google.com");
+                    }
             </pre>
         </div>
     </div>
